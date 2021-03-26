700ae5ca6a6f5ab90d330e4834c549cf0fac00c2
ambrona <mac.ambrona@gmail.com>
2021-03-26 22:12:37 +0900

    Weak move selector

--- myBot.py
+++ myBot.py
@@ -17,6 +17,8 @@ NAME  = 'demegara'
 API   = 'https://lichess.org/api/'
 AUTH  = { 'Authorization': 'Bearer ' + TOKEN }
 
+STOCKFISH = chess.engine.SimpleEngine.popen_uci('./Stockfish/src/stockfish')
+
 
 def challenge_user(username, rated = False, seconds = 300, increment = 0):
     '''
@@ -48,6 +50,19 @@ def cancel_challenge(gameID):
     '''
 
     answer = requests.post(API + 'challenge/' + gameID + '/cancel', headers = AUTH)
+    info = json.loads(answer.text)
+
+
+def accept_challenge(gameID):
+    '''
+      Accept a challenge.
+        Input:
+             gameID (challenge identifier)
+
+        No output
+    '''
+
+    answer = requests.post(API + 'challenge/' + gameID + '/accept', headers = AUTH)
     info = json.loads(answer.text)
 
 
@@ -89,11 +104,13 @@ def wait_for_starting_game(gameID):
     return False
 
 
-def play_game(gameID, moveSelector):
+def play_game(gameID, moveSelector, chatMessenger):
     '''
       Play a game which has started.
         Input:
-             gameID (game identifier)
+                gameID (game identifier)
+         moveoSelector (function that takes a chess.Board and outputs a chess.Board.Move.from_uci)
+         chatMessenger (function that takes a chess.Board and outputs a string or None)
 
         No output
     '''
@@ -107,6 +124,10 @@ def play_game(gameID, moveSelector):
                 print(line, flush = True)
                 info = json.loads(line)
                 state = info.get('state')
+
+                # Check if we got a message from the chat and continue in that case
+                if info.get('type') == 'chatLine':
+                    continue
 
                 # Get the color our BOT is playing with
                 if state:
@@ -128,6 +149,12 @@ def play_game(gameID, moveSelector):
                     for move in moves.split(' '):
                         b.push_uci(move)
 
+                # Possibly write in the chat
+                msg = chatMessenger(b)
+                if msg:
+                    parameters = { 'room' : 'player', 'text' : msg }
+                    requests.post(API + 'bot/game/' + gameID + '/chat', headers = AUTH, data = parameters)
+
                 # Continue the loop if it is not the BOT's turn
                 if b.turn != color:
                     continue
@@ -140,7 +167,7 @@ def play_game(gameID, moveSelector):
     s.close()
 
 
-def randomMove(board):
+def random_move(board):
     '''
       Pick a random move from the given board
         Input:
@@ -152,10 +179,141 @@ def randomMove(board):
     return random.choice([m for m in board.legal_moves])
 
 
+def analyze(board, searchDepth = 10, outputInfo = False, multiPV = 1):
+    '''
+      Pick a move using Stockfish at a low depth
+        Input:
+             board (chess.Board type, from python-chess)
+
+        Output:
+             move (chess.Move.from_uci type, from python-chess)
+    '''
+
+    global STOCKFISH
+    limit = chess.engine.Limit(depth = searchDepth)
+
+    info = STOCKFISH.analyse(board, limit, multipv = multiPV)
+    output = []
+
+    for info_move in info:
+
+        try:
+            best = info_move['pv'][0]
+        except:
+            best = None
+
+        score = info_move['score'].relative.score(mate_score = 10**6)
+
+        if outputInfo:
+            output.append({ 'move' : best, 'score' : score })
+
+        else:
+            output.append(best)
+
+    if multiPV > 1:
+        return output
+    else:
+        return output[0]
+
+
+def weak_engine(board):
+    '''
+      Weak engine that picks a move
+        Input:
+             board (chess.Board type, from python-chess)
+
+        Output:
+             move (chess.Move.from_uci type, from python-chess)
+    '''
+
+    # If there is only a good move, play it
+
+    moves = analyze(board, searchDepth = 10, outputInfo = True, multiPV = 2)
+
+    if len(moves) == 1 or moves[0]['score'] - moves[1]['score'] > 300:
+        return moves[0]['move']
+
+    u = random.random()
+
+    if u < 2/3:
+        return analyze(board, 2)
+
+    else:
+        return random_move(board)
+
+
+def simpleMessenger(board):
+
+    return None
+
+    messages = [
+        "Hola, soy un robotito!",
+        "Ahora seremos amigos para siempre",
+        "Te parece bien?",
+        None, None, None,
+        "Genial!",
+        None,
+        "Me iban a llamar IJO, pero mi creador pensó que BOT IJO no quedaba muy bien",
+        None, None,
+        "Gracias por jugar conmigo",
+        None, None,
+        "Mi cumpleaños es el 18 de marzo, acuérdate de felicitarme",
+        "Creo que @analyze es una gran jugada",
+        "Pero no me hagas mucho caso"
+    ]
+
+    n = board.fullmove_number - 1
+
+    if not board.turn and n < len(messages):
+
+        msg = messages[n]
+
+        if msg and "@analyze" in msg:
+            move = analyze(board, searchDepth = 10)
+            msg = msg.replace("@analyze", board.san(move))
+
+        return msg
+
+
+def wait_for_challenges():
+    '''
+      Continuously wait for challenges.
+      When a challenge comes, accept it, play the game and continue.
+        No input
+        No output
+    '''
+
+    s = requests.Session()
+    with s.get(API + 'stream/event', headers = AUTH, stream = True) as answer:
+
+        for line in answer.iter_lines():
+            if line:
+                info = json.loads(line)
+                print(info, flush = True)
+
+                if info.get('type') == 'challenge':
+
+                    gameID = info.get('challenge').get('id')
+                    accept_challenge(gameID)
+                    play_game(gameID, weak_engine, simpleMessenger)
+
+                if info.get('type') == 'gameStart':
+
+                    gameID = info.get('game').get('id')
+                    play_game(gameID, weak_engine, simpleMessenger)
+    s.close()
+
+
 if __name__ == '__main__':
 
-    user = sys.argv[1]
-    gameID = challenge_user(user, increment = 3, rated = False)
+    try:
+        user = sys.argv[1]
+        gameID = challenge_user(user, increment = 0, rated = True)
 
-    if wait_for_starting_game(gameID):
-        play_game(gameID, randomMove)
+        if wait_for_starting_game(gameID):
+            play_game(gameID, weak_engine, simpleMessenger)
+
+    except:
+        wait_for_challenges()
+
+    STOCKFISH.close()
